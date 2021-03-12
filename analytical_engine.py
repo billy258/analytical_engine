921217c4bdebfdb186cef9c0bfb5c20fc5e10474
william irving <64707280+goodtime258@users.noreply.github.com>
2021-03-12 18:17:58 +0000

    made the varbibles more readable and formatted the file

--- analytical_engine.py
+++ analytical_engine.py
@@ -1,35 +1,37 @@
-addend = [0 for i in range(40)]
-carry = [0 for i in range(40)]
-accumulator = [0 for i in range(40)]
-
-#This is the machinery of the machine
-def increment_adder(addend, accumulator, carry):
-#This reduces the addend by 1   
-    addend = addend - 1
- #This increments accumulator by one   
-    accumulator = (accumulator + 1) % 10
-#If the accumulator is 0 increment the carry
-    if accumulator == 0:
-        carry = carry + 1
-    return addend, accumulator, carry
-
-def add_digits(addend, accumulator):
-    carry = 0
-
-    while addend > 0:
-        [addend, accumulator, carry] = increment_adder(addend, accumulator, carry)
-
-    return accumulator, carry
-
-acc = input('enter integar less than 41 digits long: ')
-addend = input('enter integar less than 41 digits long: ')
-
-for index, digit in enumerate(acc):
-    accumulator[len(acc) - digit] = digit
-for index, digit in enumerate(addend):
-    addend[len(addend) - index] = digit
+addend = [0 for a in range(40)]
+carry = [0 for b in range(40)]
+accumulator = [0 for c in range(40)]
 
 
-print(acc, addend, sep = ' ')
-print(add_digits(addend, acc))
+# This is the machinery of the machine
+def increment_adder(add, acc, c):
+    # This reduces the addend by 1
+    add = add - 1
+    # This increments accumulator by one
+    acc = (acc + 1) % 10
+    # If the accumulator is 0 increment the carry
+    if acc == 0:
+        c = c + 1
+    return add, acc, c
 
+
+def add_digits(add, acc):
+    c = 0
+
+    while add > 0:
+        [add, acc, c] = increment_adder(add, acc, c)
+
+    return acc, c
+
+
+ac = input('enter intergar less than 41 digits long: ')
+ae = input('enter intergar less than 41 digits long: ')
+
+for index, digit in enumerate(ac):
+    accumulator[len(ac) - index] = int(digit)
+for index, digit in enumerate(ae):
+    addend[len(addend) - index] = int(digit)
+
+print(accumulator, addend, sep=' ')
+print(accumulator, addend, sep=' ')
+print(add_digits(addend, accumulator))
