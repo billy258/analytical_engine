f7e3b553859970976257663ceb8dfbe9d0a6e054
william irving <64707280+goodtime258@users.noreply.github.com>
2021-03-16 12:26:34 +0000

    got rid of typos

--- analytical_engine.py
+++ analytical_engine.py
@@ -1,21 +1,12 @@
-addend = [0 for a in range(40)]
-carry = [0 for b in range(40)]
-accumulator = [0 for c in range(40)]
-
-
-# This is the machinery of the machine
 def increment_adder(add, acc, c):
-    # This reduces the addend by 1
     add = add - 1
-    # This increments accumulator by one
     acc = (acc + 1) % 10
-    # If the accumulator is 0 increment the carry
     if acc == 0:
         c = c + 1
     return add, acc, c
 
 
-def subtracter(add, acc, c):
+def decrement_numbers(add, acc, c):
     add -= 1
     if acc <= 0:
         c += 1
@@ -27,7 +18,7 @@ def subtracter(add, acc, c):
 def subtract_digits(add, acc):
     c = 0
     while add > 0:
-        [add, acc, c] = subtracter(add, acc, c)
+        [add, acc, c] = decrement_numbers(add, acc, c)
     return acc, c
 
 
@@ -48,7 +39,7 @@ def addition_mill(add):
 def subtraction_mill(add):
     for i, d in reversed(list(enumerate(add))):
         total, remainder = subtract_digits(d, accumulator[i])
-        accumulator[i] = total % 10
+        accumulator[i] = total
         carry[i-1] = remainder
 
 
@@ -57,21 +48,27 @@ def axes_maker(string, axes):
         axes[-1 - wheel] = int(digit)
 
 
-ac = input('enter intergar less than 41 digits long (accumulator): ')
-ae = input('enter intergar less than 41 digits long(addend): ')
+addend = [0 for a in range(40)]
+carry = [0 for b in range(40)]
+accumulator = [0 for c in range(40)]
+
+ac = input('enter integer less than 41 digits long (accumulator): ')
+ae = input('enter integer less than 41 digits long(addend): ')
 inp = input('adding or subtracting? +/-: ')
 
 axes_maker(ac, accumulator)
 axes_maker(ae, addend)
 
-print('accumulators: ', accumulator, 'addend: ', addend, sep="\n")
+print('accumulator: ', accumulator, 'addend: ', addend, sep="\n")
 
 if inp == '+':
     addition_mill(addend)
+    print('carry: ', carry)
     for index, c in enumerate(carry):
         accumulator[index] += c
 if inp == '-':
     subtraction_mill(addend)
+    print('carry: ', carry)
     for index, c in enumerate(carry):
         accumulator[index] -= c
 
